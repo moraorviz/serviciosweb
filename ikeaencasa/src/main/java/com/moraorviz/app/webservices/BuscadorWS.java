38eaf1b2b6781233f78d829c8bccb0a818979d35
Mario <moraorviz@gmail.com>
2019-02-11 00:11:02 +0100

    defined search ws

---
+++ ikeaencasa/src/main/java/com/moraorviz/app/webservices/BuscadorWS.java
@@ -0,0 +1,15 @@
+package com.moraorviz.app.webservices;
+
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "com.moraorviz.app.webservices.IBuscadorWS")
+public class BuscadorWS implements IBuscadorWS {
+
+	public double search(String tipo, String nombre) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
