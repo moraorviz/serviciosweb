f962f9cf6dddb9da90c13b6ea090b1599a95bc1e
Mario <moraorviz@gmail.com>
2019-02-11 01:18:25 +0100

    searcher ws tested

--- ikeaencasa/src/main/java/com/moraorviz/app/webservices/BuscadorWS.java
+++ ikeaencasa/src/main/java/com/moraorviz/app/webservices/BuscadorWS.java
@@ -1,12 +1,35 @@
 package com.moraorviz.app.webservices;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.WebService;
 
 @WebService(endpointInterface = "com.moraorviz.app.webservices.IBuscadorWS")
 public class BuscadorWS implements IBuscadorWS {
-
+	
+	public static List<Mueble> mockInventario = new ArrayList<>();
+	
+	public BuscadorWS() {
+		mockInventario.add(new Mueble("mueble", "LACK", 59));
+		mockInventario.add(new Mueble("mueble", "BYAS", 69));
+		mockInventario.add(new Mueble("cambiador", "SNIGLAR", 25));
+		mockInventario.add(new Mueble("cuna", "SNIGLAR", 49));
+		mockInventario.add(new Mueble("sillon orejero", "STRANDMON", 179));
+		mockInventario.add(new Mueble("almohada firmeza media", "ROSENSTJARNA", 10));
+		mockInventario.add(new Mueble("almohada viscoelastica", "MANVIVA", 29.99));
+		mockInventario.add(new Mueble("foco triple con base", "VITEMOLLA", 49.99));
+		mockInventario.add(new Mueble("lampara de pared", "LILLHOLMEN", 16.99));
+	}
+	
+	
 	public double search(String tipo, String nombre) {
 		// TODO Auto-generated method stub
+		for (Mueble m: mockInventario) {
+			if (m.tipo.contentEquals(tipo) && m.nombre.contentEquals(nombre)) {
+				return m.precio;
+			}
+		}
 		return 0;
 	}
 	
---
+++ ikeaencasa/src/main/java/com/moraorviz/app/webservices/Mueble.java
@@ -0,0 +1,16 @@
+package com.moraorviz.app.webservices;
+
+public class Mueble {
+
+	public String tipo;
+	public String nombre;
+	public double precio;
+	
+	public Mueble(String tipo, String nombre, double precio) 
+	{	
+		this.tipo = tipo;
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
+}
